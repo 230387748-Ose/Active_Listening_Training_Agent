55398bb5d73f70ed011525e910fbb91b0347b813
230387748-Ose <230387748@aston.ac.uk>
2024-08-30 12:33:47 +0100

    added audio delay to character 1

---
+++ Assets/Scripts/AudioDelay.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CrazyMinnow.SALSA; // Ensure this is the correct namespace for SALSA
+
+public class AudioDelay : MonoBehaviour
+{
+    public AudioSource audioSource;  // Assign the AudioSource in the Inspector
+    public float delay = 2.0f;  // Time in seconds to delay the audio
+
+    void Start()
+    {
+        // Play the audio with a delay
+        audioSource.PlayDelayed(delay);
+    }
+}
